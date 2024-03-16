be5a1e4f39ba878714564549660726defff438b7
zizifn <1803942+zizifn@users.noreply.github.com>
2024-03-16 19:01:21 +0800

    remove template

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -94,9 +94,9 @@ const config: Config = {
           position: "left",
           label: "Setup",
         },
-        { to: "/blog", label: "Blog", position: "left" },
+        // { to: "/blog", label: "Blog", position: "left" },
         {
-          href: "https://github.com/facebook/docusaurus",
+          href: "https://github.com/zizifn/gama",
           label: "GitHub",
           position: "right",
         },
