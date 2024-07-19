fac58d2c311e381f59426d74207e6e56439a5b95
zizifn <1803942+zizifn@users.noreply.github.com>
2024-07-19 15:47:56 +0800

    更新 index.tsx

--- src/components/EndpointChecker/index.tsx
+++ src/components/EndpointChecker/index.tsx
@@ -27,7 +27,7 @@ export function CheckAvailability({
           Authorization: `Bearer ${apikey}`,
         },
         body: JSON.stringify({
-          model: "gpt-3.5-turbo",
+          model: "gpt-4o-mini",
           messages: [
             {
               role: "user",
